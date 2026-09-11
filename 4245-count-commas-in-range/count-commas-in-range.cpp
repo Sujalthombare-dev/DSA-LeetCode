f5fb956d8a1458c06700096fc2eae9bb1517d158
Sujalthombare-dev <sujaleducational@gmail.com>
2026-09-11 19:09:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.28%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if (n>=1000){
+            return 1+(n-1000);
+
+        }else{
+            return 0;
+        }
+        return 0;
+        
+    }
+};
